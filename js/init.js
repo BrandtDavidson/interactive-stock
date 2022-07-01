26159a99164014d458fc3b76daa5d9b2881e9386
smulrow <smulrow@gmail.com>
2022-07-01 10:09:00 -0700

    adding functionality to news section

--- js/init.js
+++ js/init.js
@@ -1,14 +1,3 @@
-// (function($){
-//   $(function(){
-
-//     $('.sidenav').sidenav();
-
-//   });
-//   instance.open(); // end of document ready
-// })(jQuery); // end of jQuery name space
-
-
-
 var searchBtn = document.getElementById("pinStock")
 var apiKey = "cbFInYyDImkzGOO66NTz9LPD0lxIYu6BpmALFthz"
 var newSearch = document.querySelector("#stockInputField")
@@ -45,10 +34,25 @@ var displayNews = function(data) {
   var headline = data.data[0].title
   var article = data.data[0].description
   var url = data.data[0].url
+  
+  var headline2 = data.data[1].title
+  var article2 = data.data[1].description
+  var url2 = data.data[1].url
+  
+  var headline3 = data.data[2].title
+  var article3 = data.data[2].description
+  var url3 = data.data[2].url
+  
 
   document.querySelector(".news").innerHTML = headline
   document.querySelector(".article").innerHTML = article
   document.querySelector(".url").innerHTML = url
+  document.querySelector(".news2").innerHTML = headline2
+  document.querySelector(".article2").innerHTML = article2
+  document.querySelector(".url2").innerHTML = url2
+  document.querySelector(".news3").innerHTML = headline3
+  document.querySelector(".article3").innerHTML = article3
+  document.querySelector(".url3").innerHTML = url3
 }
 
 searchBtn.addEventListener("click" , formSubmitHandler)
